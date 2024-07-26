9cf1c8a8a3c4e2665962228d35c9a6d16c478aba
Pratik.Shrivastav <Pratik.Shrivastav@DELL1615>
2024-07-27 00:54:15 +0530

    Add project files.

---
+++ BmiApp/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmiApp
+{
+    public class Person
+    {
+        private int _personId;
+        private string _personName;
+        private int _personAge;
+        private double _personHeight;
+        private double _personWeight;
+        public const double DEFAULT_WEIGHT = 50;
+        public const double DEFUALT_HEIGHT = 5;
+        public const double TO_METER = 0.3048;
+
+
+        public Person(int personId, string personName, int personAge) 
+            : this(personId, personName, personAge , DEFUALT_HEIGHT, DEFAULT_WEIGHT)
+        { 
+        }
+        public Person(int personId, string personName, int personAge, double personHeight, double personWeight) 
+        {
+            _personId = personId;
+            _personName = personName;
+            _personAge = personAge;
+            _personHeight = TO_METER * personHeight;
+            _personWeight = personWeight;
+        }
+
+        public String CalculateBmiCriteria()
+        {
+            double bmi = CalculateBmi();
+            string bmiCriteria = "";
+            if (bmi <= 18.5) { bmiCriteria = "Under Weight"; }
+            if (bmi > 18.5 && bmi <= 24.9) { bmiCriteria = "Normal Weight"; }
+            if (bmi >= 25 && bmi <= 29.9) { bmiCriteria = "Over Weight"; }
+            if (bmi >= 30) { bmiCriteria = "Obese "; }
+            return $"The Bmi of {_personName} is {bmi} and the person is {bmiCriteria}";
+        }
+        public double CalculateBmi()
+        {
+            double bmi = _personWeight / (_personHeight * _personHeight);
+            return bmi;
+        }
+
+        public string PrintDetails()
+        {
+            return $"The id of person is {_personId}" +
+                $"\nThe Name of person is {_personName}" +
+                $"\nThe Age of person is {_personAge}" +
+                $"\nThe Height of Person is {_personHeight}" +
+                $"\nThe Weight of Person is {_personWeight}";
+        }
+    }
+}
---
+++ BmiApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmiApp
+{
+    public class Program
+    {
+        public static void MaxBmi(Person[] persons)
+        {
+            double maxBmi = 0;
+            Person maxBmiPerson = null;
+            foreach (Person person in persons)
+            {
+                double compareBmi = person.CalculateBmi();
+                if (maxBmi < compareBmi)
+                {
+                    maxBmi = compareBmi;
+                    maxBmiPerson = person;
+                }
+            }
+            Console.WriteLine("The Max BMI is {0}", maxBmi);
+            Console.WriteLine(maxBmiPerson.PrintDetails());
+        }
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1, "Pratik", 22, 6, 75);
+            Person person2 = new Person(2, "Ravi", 24, 5.4, 90);
+            Person person3 = new Person(3, "Suraj", 24);
+
+            Person[] persons = { person1, person2, person3 };
+
+            MaxBmi(persons);
+
+            Console.WriteLine(person1.CalculateBmiCriteria());
+            Console.WriteLine(person2.CalculateBmiCriteria());
+            Console.WriteLine(person3.CalculateBmiCriteria());
+        }
+    }
+}
